8d54fbd7c363df3ba36ce3cfa06ebd00c13d92e9
MaansiBotke <maansibotke2298@gmail.com>
2020-07-12 17:45:35 +0530

    Adding Covid image and device responsive styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import corona_image from './images/covid-image.png';
 
 class App extends React.Component {
   
@@ -31,6 +32,7 @@ class App extends React.Component {
     
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={corona_image} alt="COVID-19" />
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Chart data={data} country={country}/>
